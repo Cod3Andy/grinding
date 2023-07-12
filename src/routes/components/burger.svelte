c2bfbece188ac23ec7f17fbd4c303ebbb8b43013
Cod3Andy <derangedjk75@gmail.com>
2023-07-13 02:31:58 +0300

    Commiting some errors

---
+++ src/routes/components/burger.svelte
@@ -0,0 +1,14 @@
+<script> 
+    const burger = document.querySelector('#burger');
+    const menu = document.querySelector('#menu');
+
+const burgerCheck = () => { 
+    burger.addEventListener('click', () => {
+    if(menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden')
+    } else {
+        menu.classList.add('hidden')
+    }
+})
+ }
+</script>
